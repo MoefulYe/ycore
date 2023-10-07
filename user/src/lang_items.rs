919a7af8cd2de93758e0d60153e3a4803791669b
ashenye <luren145@gmail.com>
2023-10-07 15:23:15 +0800

    x

---
+++ user/src/lang_items.rs
@@ -0,0 +1,20 @@
+use core::panic::PanicInfo;
+
+use crate::exit;
+
+#[panic_handler]
+fn panic_handler(panic_info: &PanicInfo) -> ! {
+    let err = panic_info.message().unwrap();
+    if let Some(location) = panic_info.location() {
+        println!(
+            "Panicked at {}:{}, {}",
+            location.file(),
+            location.line(),
+            err
+        );
+    } else {
+        println!("Panicked: {}", err);
+    }
+    exit(-1);
+    loop {}
+}
