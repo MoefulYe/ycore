6fd9721fb89fe0261d45aeda8945fcbb42072700
ashenye <luren145@gmail.com>
2023-11-01 16:39:12 +0800

    modify layout

--- yfs/src/layout.rs
+++ yfs/src/layout.rs
@@ -1,3 +1,9 @@
+use core::mem::size_of;
+
+use alloc::sync::Arc;
+
+use crate::{constant::{BlockAddr, BLOCK_SIZE, BLOCK_BITS, NULL, Block}, block_dev::BlockDevice, block_cache::BLOCK_CACHE};
+
 #[repr(C)]
 pub struct SuperBlock {
     pub magic: u32,
@@ -40,4 +46,57 @@ impl SuperBlock {
     pub fn valid(&self) -> bool {
         self.magic == Self::MAGIC
     }
+}
+
+
+const INODE_DIRECT_COUNT: u32 = 28;
+const INDIRECT1_COUNT: u32 = BLOCK_BITS / 4;
+const INDIRECT2_COUNT: u32 = INDIRECT1_COUNT * INDIRECT1_COUNT;
+const INDIRECT1_BOUND: u32 = INDIRECT1_COUNT + INODE_DIRECT_COUNT;
+const INDIRECT2_BOUND: u32 = INDIRECT2_COUNT + INDIRECT1_BOUND;
+type IndexBlock = [BlockAddr; BLOCK_SIZE/size_of<BlockAddr>()];
+
+#[repr(C)]
+pub struct Inode {
+    pub size: u32,
+    pub direct: [u32; INODE_DIRECT_COUNT],
+    pub indirect1: u32,
+    pub indirect2: u32,
+    inode_type: InodeType,
+}
+
+impl Inode {
+    pub fn init(&mut self, inode_type: InodeType) {
+        self.size = 0;
+        self.inode_type = inode_type;
+        self.direct = [NULL; INODE_DIRECT_COUNT];
+        self.indirect1 = NULL;
+        self.indirect2 = NULL;
+    }
+
+    pub fn is_file(&self) -> bool {
+        self.inode_type == InodeType::File
+    }
+
+    pub fn is_dir(&self) -> bool {
+        self.inode_type == InodeType::Dir
+    }
+
+    pub fn nth_data_block(&self, n: u32, device: &Arc<dyn BlockDevice>) -> BlockAddr {
+        if n < INODE_DIRECT_COUNT {
+            return self.direct[n]
+        } else if n < INDIRECT1_BOUND {
+            BLOCK_CACHE
+                .lock()
+                .get_cache(self.indirect1, Arc::clone(device))
+                .lock()
+                .read(0, |block: &Block| block[n - INODE_DIRECT_COUNT])
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum InodeType {
+    File,
+    Dir,
 }
