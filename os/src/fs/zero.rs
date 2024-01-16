d2c54181bc7aaa7c5ad2a2944ec08dd483a79b40
ashenye <luren145@gmail.com>
2024-01-16 21:21:04 +0800

    x

---
+++ os/src/fs/zero.rs
@@ -0,0 +1,39 @@
+use alloc::sync::Arc;
+
+use super::File;
+
+struct Zero;
+
+impl File for Zero {
+    fn readable(&self) -> bool {
+        true
+    }
+
+    fn writable(&self) -> bool {
+        true
+    }
+
+    fn seekable(&self) -> bool {
+        true
+    }
+
+    fn read(&self, buf: crate::mm::address::UserBuffer) -> isize {
+        let len = buf.len();
+        for buf in buf {
+            buf.fill(0);
+        }
+        len as isize
+    }
+
+    fn write(&self, buf: crate::mm::address::UserBuffer) -> isize {
+        buf.len() as isize
+    }
+
+    fn seek(&self, _: super::SeekType, _: i32) -> isize {
+        0
+    }
+}
+
+pub fn zero() -> Arc<dyn File + Send + Sync> {
+    Arc::new(Zero)
+}
