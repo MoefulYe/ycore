d9b32b4b8361580f0145db304a2ad8bfa5651321
ashenye <luren145@gmail.com>
2024-01-21 12:22:34 +0800

    x

--- user/src/bin/custom_action.rs
+++ user/src/bin/custom_action.rs
@@ -11,10 +11,13 @@ fn action() -> ! {
 }
 
 #[no_mangle]
-fn main(_: &Argv) -> i32 {
+fn main(argv: &Argv) -> i32 {
+    assert!(argv.len() == 2);
     let pid = getpid();
-    let action = SignalAction::new(action, SignalFlags::empty());
-    sig_setaction(SIGUSR2, action);
+    if argv[1] == "set" {
+        let action = SignalAction::new(action, SignalFlags::empty());
+        sig_setaction(SIGUSR2, action);
+    }
     kill(pid, SIGUSR2).unwrap();
     println!("hello world");
     0
